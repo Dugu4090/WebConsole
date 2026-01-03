0e9d80d48b52a6b419ed160890d8dcb5a74f1dc2
root <root@hostforge-vps-1350423223380082728-1.lxd>
2026-01-03 03:58:29 +0000

    Remove embedded HTTP server, keep WebSocket only

--- src/es/mesacarlos/webconsole/WebConsole.java
+++ src/es/mesacarlos/webconsole/WebConsole.java
@@ -2,10 +2,8 @@ package es.mesacarlos.webconsole;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.security.KeyStore;
-import java.util.concurrent.Executors;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
@@ -19,42 +17,27 @@ import es.mesacarlos.webconsole.config.ConfigManager;
 import es.mesacarlos.webconsole.minecraft.WebConsoleCommand;
 import es.mesacarlos.webconsole.util.Internationalization;
 import es.mesacarlos.webconsole.util.TpsTracker;
-import es.mesacarlos.webconsole.web.StaticHandler;
 import es.mesacarlos.webconsole.websocket.WSServer;
-
-import com.sun.net.httpserver.HttpServer;
 
 public class WebConsole extends JavaPlugin {
     
     private WSServer server;
     private Thread wsThread;
-    private HttpServer httpServer;
-    private Thread httpThread;
 
     @Override
     public void onEnable() {
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TpsTracker(), 100L, 1L);
         Internationalization.setCurrentLocale(ConfigManager.getInstance().getLanguage());
         
-        // Start WebSocket Server for console
         try {
             startWS();
         } catch (Exception e) {
             Bukkit.getLogger().warning("[WebConsole] Failed to start WebSocket server: " + e.getMessage());
         }
-        
-        // Start HTTP Server for web dashboard
-        try {
-            startHTTPServer();
-        } catch (Exception e) {
-            Bukkit.getLogger().warning("[WebConsole] Failed to start HTTP server: " + e.getMessage());
-        }
 
-        // Log filter for console
         org.apache.logging.log4j.core.Filter f = new es.mesacarlos.webconsole.util.LogFilter(getWSServer());
         ((org.apache.logging.log4j.core.Logger) org.apache.logging.log4j.LogManager.getRootLogger()).addFilter(f);
         
-        // Register /WebConsole command
         getCommand("WebConsole").setExecutor(new WebConsoleCommand(this.getDescription().getVersion()));
     }
 
@@ -63,18 +46,31 @@ public class WebConsole extends JavaPlugin {
         try {
             if (server != null) server.stop();
             if (wsThread != null) wsThread = null;
-            if (httpServer != null) httpServer.stop(0);
-            if (httpThread != null) httpThread = null;
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * Start WebSocket server for console functionality
-     */
     private void startWS() throws Exception {
-        server = new WSServer(ConfigManager.getInstance().getSocketAdress());
+        int configuredPort = ConfigManager.getInstance().getSocketAdress().getPort();
+        
+        int[] wsPortsToTry = {configuredPort, configuredPort + 1, configuredPort + 2, 8070, 8080, 8090, 9000};
+        
+        for (int port : wsPortsToTry) {
+            try {
+                InetSocketAddress address = new InetSocketAddress(port);
+                WSServer testServer = new WSServer(address);
+                server = testServer;
+                break;
+            } catch (Exception e) {
+                Bukkit.getLogger().warning("[WebConsole] Port " + port + " is in use, trying next...");
+                continue;
+            }
+        }
+        
+        if (server == null) {
+            throw new Exception("Failed to start WebSocket server on any port");
+        }
         
         if (ConfigManager.getInstance().isSslEnabled()) {
             String STORETYPE = ConfigManager.getInstance().getStoreType();
@@ -109,48 +105,6 @@ public class WebConsole extends JavaPlugin {
         
         Bukkit.getLogger().info("[WebConsole] WebSocket server started on port " + 
             ConfigManager.getInstance().getSocketAdress().getPort());
-    }
-
-    /**
-     * Start HTTP server for embedded web dashboard
-     */
-    private void startHTTPServer() throws IOException {
-        int httpPort = ConfigManager.getInstance().getPort();
-        
-        // Try to create HTTP server, fallback to alternative ports if needed
-        HttpServer server = null;
-        int[] portsToTry = {httpPort, httpPort + 1, httpPort + 2, 8080, 8090, 8100};
-        
-        for (int port : portsToTry) {
-            try {
-                server = HttpServer.create(new InetSocketAddress(port), 0);
-                httpPort = port;
-                break;
-            } catch (java.net.BindException e) {
-                Bukkit.getLogger().warning("[WebConsole] Port " + port + " is in use, trying next port...");
-                continue;
-            }
-        }
-        
-        if (server == null) {
-            Bukkit.getLogger().severe("[WebConsole] Failed to start HTTP server: all ports in use");
-            return;
-        }
-        
-        httpServer = server;
-        httpServer.setExecutor(Executors.newCachedThreadPool());
-        
-        // Register static file handler
-        httpServer.createContext("/", new StaticHandler());
-        
-        httpServer.start();
-        
-        if (httpPort == ConfigManager.getInstance().getPort()) {
-            Bukkit.getLogger().info("[WebConsole] Web dashboard started on http://0.0.0.0:" + httpPort);
-        } else {
-            Bukkit.getLogger().warning("[WebConsole] Web dashboard started on http://0.0.0.0:" + httpPort + " (configured port was " + ConfigManager.getInstance().getPort() + ")");
-        }
-        Bukkit.getLogger().info("[WebConsole] Access the dashboard at: http://YOUR_SERVER_IP:" + httpPort);
     }
 
     public WSServer getWSServer() {
